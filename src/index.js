983041aeed1dcc5fe6c8906d1ea7fd356e49d04f
Maria <workmail358@gmail.com>
2024-09-28 22:44:55 +0300

    передеално открытие меню

--- src/index.js
+++ src/index.js
@@ -30,37 +30,66 @@ const button = document.querySelector(".burger-menu");
 
 button.addEventListener("click", () => {
 	button.classList.toggle("active");
+	document.body.classList.toggle("open");
+	document.querySelector('.header').classList.toggle("open");
+
 });
 
 
 
-function isDesktop() {
-  return !('ontouchstart' in window) || !('onmsgesturechange' in window);
+// function isDesktop() {
+//   return !('ontouchstart' in window) || !('onmsgesturechange' in window);
+// }
+
+if (window.innerWidth >= 1024) {
+  console.log(window.innerWidth)
+  document.addEventListener('wheel', function(event) {
+    if (event.deltaY > 0) {
+        window.scrollBy({
+            top: window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+        body.classList.add('scrolling');
+    } else {
+        window.scrollBy({
+            top: -window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+        body.classList.remove('scrolling');
+    }
+  });
+
+  document.addEventListener('scroll', function() {
+    const scrolled = window.pageYOffset;
+    const num = -1612;
+    document.querySelector('.body-bg').style.backgroundPositionY = num - -(scrolled / 24 ) + 'px';
+  });
 }
 
-if (isDesktop()) {
-document.addEventListener('wheel', function(event) {
-  if (event.deltaY > 0) {
-      window.scrollBy({
-          top: window.innerHeight,
-          left: 0,
-          behavior: 'smooth'
-      });
-      body.classList.add('scrolling');
-  } else {
-      window.scrollBy({
-          top: -window.innerHeight,
-          left: 0,
-          behavior: 'smooth'
-      });
-      body.classList.remove('scrolling');
-  }
-});
+// if (isDesktop()) {
+// document.addEventListener('wheel', function(event) {
+//   if (event.deltaY > 0) {
+//       window.scrollBy({
+//           top: window.innerHeight,
+//           left: 0,
+//           behavior: 'smooth'
+//       });
+//       body.classList.add('scrolling');
+//   } else {
+//       window.scrollBy({
+//           top: -window.innerHeight,
+//           left: 0,
+//           behavior: 'smooth'
+//       });
+//       body.classList.remove('scrolling');
+//   }
+// });
 
-window.addEventListener('scroll', function() {
-  const scrolled = window.pageYOffset;
-  const num = -1612;
-  console.log(scrolled)
-  document.querySelector('.body-bg').style.backgroundPositionY = num - -(scrolled / 24 ) + 'px';
-});
-}
+// window.addEventListener('scroll', function() {
+//   const scrolled = window.pageYOffset;
+//   const num = -1612;
+//   document.querySelector('.body-bg').style.backgroundPositionY = num - -(scrolled / 24 ) + 'px';
+// });
+// }
